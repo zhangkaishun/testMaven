292f1a6361b424b77c57ed96cab5f65427438dec
zhangkaishun <Administrator@E2FMGZ0BXDWTQMG>
2016-01-17 17:15:39 +0800

    UserController

---
+++ src/main/java/com/zhangkaishun/user/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.zhangkaishun.user.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.zhangkaishun.user.domain.User;
+import com.zhangkaishun.user.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping("/get")
+	public ModelAndView getUser(HttpServletResponse response) {
+		User user=userService.getById(1);
+		System.out.println(user);
+		return null;
+	}
+}
